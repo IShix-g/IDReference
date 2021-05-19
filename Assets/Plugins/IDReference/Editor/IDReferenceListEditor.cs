e67c55071a331f6133daf8801efcc323b0eaccfe
IShix <>
2021-05-19 15:23:03 +0900

    Fixed export CSV

--- Assets/Plugins/IDReference/Editor/IDReferenceListEditor.cs
+++ Assets/Plugins/IDReference/Editor/IDReferenceListEditor.cs
@@ -116,7 +116,7 @@ namespace IDRef.Internal
                 
                 if (GUILayout.Button("Export CSV", style))
                 {
-                    var names = list.References.Select(x => x.Name);
+                    var names = list.References.Select(x => $"{x.ID},{x.Name}");
                     IDReferenceExportCsv.Export(list.TableID, names);
                 }
                 
